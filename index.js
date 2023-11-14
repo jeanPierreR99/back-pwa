87f7931c8a23c3508cf7a9c20d34e67a454f992a
jeanPierreR99 <kreyesf@unamad.edu.pe>
2023-11-14 09:44:28 -0500

    add node wordker

--- index.js
+++ index.js
@@ -2,47 +2,43 @@ const express = require("express");
 const app = express();
 const webpush = require('web-push');
 const cors = require("cors")
+const PushNotifications = require('@pusher/push-notifications-server');
 
 const port = 3000;
-
-const apiKeys = {
-    subject: "mailto:jeanpierrereyes19@gmail.com",
-    publicKey: "BP88K4wlPtmGx8nrjUt3YK7PI1XCT2Em6nldUEWEHoxlYXMmAiHQSLXRLXnECATYB9PadeIsEgXmKEA_rQZh7_Q",
-    privateKey: "cXXnmgQ8igENaB3Yuf0dJrdGEXFZ7japQ0RarfW8aWA"
-}
-
-webpush.setVapidDetails(
-    apiKeys.subject,
-    apiKeys.publicKey,
-    apiKeys.privateKey
-)
+const beamsClient = new PushNotifications({
+    instanceId: 'b17646f6-89ff-4959-83cd-ec8e141b6afc',
+    secretKey: 'F6E7DFE46F336E194DBE5E858908BEF05A04C5795E85C65619BF52404D7A0224',
+  });
+  
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
-})
-
-const subDatabse = [];
-
-
-app.post("/save-subscription", (req, res) => {
-    console.log("suscribiendo")
-    subDatabse.push(req.body);
-    res.json({ status: "Success", message: "Subscription saved!" })
+    res.send("GOOD");
 })
 
 app.get("/send-notification", (req, res) => {
-    const subscription = subDatabse[0];
-    console.log("Subscription:", subscription);
+    const { body, message } = req.query
 
-    if (subscription) {
-        webpush.sendNotification(subscription, "Hello world");
-        res.json({ "status": "Success", "message": "Message sent to push service" });
-    } else {
-        res.status(400).json({ "status": "Error", "message": "No subscription found" });
-    }
+    console.log("body: "+body+" message: "+message)
+
+    beamsClient.publishToInterests(['hello'], {
+        web: {
+          notification: {
+            title: body,
+            body: message,
+          },
+        },
+      })
+      .then((publishResponse) => {
+        console.log('Notificación enviada:', publishResponse);
+        res.json({response: "success"})
+      })
+      .catch((error) => {
+        console.error('Error al enviar la notificación:', error);
+        res.json({response: "error"})
+      });
 })
 
 
